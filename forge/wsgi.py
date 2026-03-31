1606f951722bed66deec44cc83085d4fcbc7e822
Ubuntu <forge@production.xunvzjwop45etmpobb4bnppimf.bx.internal.cloudapp.net>
2026-03-31 01:57:03 +0000

    moving files to new repository

--- forge/wsgi.py
+++ forge/wsgi.py
@@ -8,13 +8,13 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append('/var/www/forge_django')
+sys.path.append('/var/www/forge/forge_django')
 
 from django.core.wsgi import get_wsgi_application
-import sys
-
-sys.path.append('/home/honakm/website')
-sys.path.append('/home/honakm/website/forge')
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forge.settings')
-
+os.environ.setdefault('GOOGLE_API_KEY', 'AIzaSyD1me8o2Z-3GgEp0tVp2F2ZQMsOoM5vlXw')
+os.environ.setdefault('SENDGRID_API_KEY', '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')
+os.environ.setdefault('SITE_URL', 'www.rpiforge.dev')
 application = get_wsgi_application()
